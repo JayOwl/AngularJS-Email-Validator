f49b60335f832e34c7fc89a97351680ab784418e
JayOwl <joel.r.benoit@gmail.com>
2016-01-03 20:25:47 -0800

    Email Validator
    Email Validator in Angular JS

---
+++ EmailValidator/Controllers/controllers.js
@@ -0,0 +1,10 @@
+/**
+ * Created by abc on 10/12/2015.
+ */
+
+
+emailAppControllers.controller('AddEmailCtrl', ['scope', 'routeParams', 'emailService',
+    function ($scope, $routeParams, emailService){
+        emailService.addEmail($scope, $routeParams.)
+
+    }])
---
+++ EmailValidator/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by abc on 10/12/2015.
+ */
+var app = angular.module('app', ['ngMessages']);
